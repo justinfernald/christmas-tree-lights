711607d15acaf3c8f756a4a6c74773958847d2cd
Justin Fernald <jfernald@my.gcu.edu>
2023-12-14 22:16:22 -0700

    -_-

--- src/components/ControlBar.tsx
+++ src/components/ControlBar.tsx
@@ -114,6 +114,7 @@ const InfoDialog = observer((props: InfoDialogProps) => {
               rel="noreferrer"
               href="https://github.com/justinfernald/christmas-tree-lights/tree/main/src/animations"
             >
+              {' '}
               animations folder
             </a>
             and add your animation then make a pull request. Try give your filename a name
